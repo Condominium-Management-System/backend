644561e74dd7163a4e174962becc0a2f9b586999
shanbel <shanbeldires7@gmail.com>
2026-08-14 17:15:14 +0300

    import routes condo,admin, user, rooms, block and use them

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ import condoRoutes from "./routes/condo.route.js";
 import blockRouter from "./routes/block.route.js"
 import roomRoutes from "./routes/room.route.js"
 import cors from "cors"
-
-
+import adminRoutes from "./routes/admin.route.js"
+import userRoutes from "./routes/user.route.js"
+import equbRoutes from "./routes/equb.route.js"
+import iddirRoutes from "./routes/iddir.route.js"
+import iddirMemberRoutes from "./routes/iddirMember.route.js"
 const app = express()
 
 app.use(cors())
@@ -21,6 +24,11 @@ app.use("/api/auth", authRouter)
 app.use("/api/condos", condoRoutes);
 app.use("/api/blocks", blockRouter);
 app.use("/api/rooms", roomRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/equbs", equbRoutes);
+app.use("/api/iddirs", iddirRoutes);
+app.use("/api/iddir-members", iddirMemberRoutes);
 app.use(notFound)
 app.use(errorHandler)
 
